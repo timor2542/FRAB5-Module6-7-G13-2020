229dfa82767b0a95d484e0c6e955a30f4778d3af
Krittamet Thawong <timor2542@gmail.com>
2020-10-01 21:03:57 +0700

    Updated 1/10/2020

--- TestMCPWM.c
+++ TestMCPWM.c
@@ -6,24 +6,25 @@
  * C Compiler   :   XC16 Complier by Microship Technology
  * Author       :   Mr.Krittamet Thawong and Mr.Wisarut Kasemphumirat KMUTT FIBO FRAB#5
  * Update       :   30 September 2020 11:27 PM UTC+7 Bangkok
+ * Test Port
  */
-#include "xc.h"
-#include <stdio.h>
-#include "configuration.h"
+#include "xc.h"                                                 // Include XC16 Complier
+#include <stdio.h>                                              // Standard I/O Library
+#include "configuration.h"                                      // Bit Configuration
 
 // Global Interrupts Enable/Disable Configuration Zone
 #define GLOBAL_INT_DISABLE __builtin_disable_interrupts()       // Disable Global Interrupts
 #define GLOBAL_INT_ENABLE __builtin_enable_interrupts()         // Enable Global Interrupts
 
 // INT0 Configuration Zone
-#define INT0_ENABLE(x) IEC0bits.INT0IE = x
-#define INT0_TRIG_EDGE(x) INTCON2bits.INT0EP = x
+#define INT0_ENABLE(x) IEC0bits.INT0IE = x                      // Enable INT0
+#define INT0_TRIG_EDGE(x) INTCON2bits.INT0EP = x                // Set Positive/Negative Edge INT0
 
 // INT1,INT2 Configuration Zone
-#define INT1_ENABLE(x) IEC1bits.INT1IE = x
-#define INT1_TRIG_EDGE(x) INTCON2bits.INT1EP = x
-#define INT2_ENABLE(x) IEC1bits.INT2IE = x
-#define INT2_TRIG_EDGE(x) INTCON2bits.INT2EP = x
+#define INT1_ENABLE(x) IEC1bits.INT1IE = x                      // Enable INT1
+#define INT1_TRIG_EDGE(x) INTCON2bits.INT1EP = x                // Set Positive/Negative Edge INT1
+#define INT2_ENABLE(x) IEC1bits.INT2IE = x                      // Enable INT2
+#define INT2_TRIG_EDGE(x) INTCON2bits.INT2EP = x                // Set Positive/Negative Edge INT2
 
 // Timer1 Configuration Zone
 #define TIMER1_PRESCALE(x) T1CONbits.TCKPS = x                  // Set Timer1 Prescaler
@@ -33,30 +34,34 @@
 #define TIMER1_ON(x) T1CONbits.TON = x                          // Enable Timer1
 
 // Timer23 Configuration Zone
-#define TIMER2_PRESCALE(x) T1CONbits.TCKPS = x                  // Set Timer2 Prescaler
+#define TIMER2_PRESCALE(x) T2CONbits.TCKPS = x                  // Set Timer2 Prescaler
 #define TIMER23_COMBINE(x) T2CONbits.T32 = x                    // Enable Combine Timer23
 #define TIMER3HB_PERIOD(x) PR3 = x                              // Set Timer3 Period
 #define TIMER2LB_PERIOD(x) PR2 = x                              // Set Timer2 Period
-#define TIMER3_INT_ENABLE(x) _T3IE = x                          // Enable Interrupt Timer1
-#define TIMER3_INT_PRIORITY(x) _T3IP = x                        // Set Priority of Interrupt Timer1
+#define TIMER3_INT_ENABLE(x) _T3IE = x                          // Enable Interrupt Timer3
+#define TIMER3_INT_PRIORITY(x) _T3IP = x                        // Set Priority of Interrupt Timer3
 #define TIMER2_ON(x) T2CONbits.TON = x                          // Enable Timer2
 
 // Timer45 Configuration Zone
-#define TIMER4_PRESCALE(x) T4CONbits.TCKPS = x                  // Set Timer2 Prescaler
-#define TIMER45_COMBINE(x) T5CONbits.T32 = x                    // Enable Combine Timer23
-#define TIMER5HB_PERIOD(x) PR5 = x                              // Set Timer3 Period
-#define TIMER4LB_PERIOD(x) PR4 = x                              // Set Timer2 Period
-#define TIMER5_INT_ENABLE(x) _T4IE = x                          // Enable Interrupt Timer1
-#define TIMER5_INT_PRIORITY(x) _T4IP = x                        // Set Priority of Interrupt Timer1
-#define TIMER4_ON(x) T4CONbits.TON = x                          // Enable Timer2
+#define TIMER4_PRESCALE(x) T4CONbits.TCKPS = x                  // Set Timer4 Prescaler
+#define TIMER45_COMBINE(x) T4CONbits.T32 = x                    // Enable Combine Timer45
+#define TIMER5HB_PERIOD(x) PR5 = x                              // Set Timer5 Period
+#define TIMER4LB_PERIOD(x) PR4 = x                              // Set Timer4 Period
+#define TIMER5_INT_ENABLE(x) _T5IE = x                          // Enable Interrupt Timer5
+#define TIMER5_INT_PRIORITY(x) _T5IP = x                        // Set Priority of Interrupt Timer5
+#define TIMER4_ON(x) T4CONbits.TON = x                          // Enable Timer4
 
 // Frequency Configuration Zone
-#define FCY 40000000UL
-#define MOTOR_PWM_FREQ 500        //motor PWM frequency in Hz
-#define MOTOR_DUTY 70           //motor duty in %
+#define FCY 40000000UL                                          // Cycle Frequency in Hz
+#define MOTOR_PWM_FREQ 500                                      // Motor PWM Frequency in Hz
+#define MOTOR_DUTY 70                                           // Motor Duty in %
 
-char __dir;
-char __STOPM1status = 0;
+#define ALL2 0x66
+#define ALL3 0x67
+#define FORWARD 0x01
+#define BACKWARD 0x02
+
+char __dir, __STOPM1status = 0;                                 //
 
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void){
     __dir ^= 0x01;
@@ -86,7 +91,7 @@ void __setIO(int __byte1,int __byte2,int __byte3){
     TRISB = __byte3;
 }
 
-void __ExINT12_MAPPING(char __selectINT, char __selectRP)
+void __ExINT_MAPPING(char __selectINT, char __selectRP)
 {
     OSCCON = 0x46;
     OSCCON = 0x57;
@@ -107,22 +112,22 @@ void initPWM()
 {
      //setup PWM ports
     
-     PWM1CON1 = 0;                  //  Clear all bits (use defaults)
+     PWM1CON1 = 0;                                          //  Clear all bits (use defaults)
      
-     PWM1CON1bits.PMOD1 = 1;        //  PWM1L1,PWM1H1 are in independent running mode
-     PWM1CON1bits.PMOD2 = 1;        //  PWM1L2,PWM1H2 are in independent running mode
-     PWM1CON1bits.PMOD3 = 1;        //  PWM1L3,PWM1H3 are in independent running mode
+     PWM1CON1bits.PMOD1 = 1;                                //  PWM1L1,PWM1H1 are in independent running mode
+     PWM1CON1bits.PMOD2 = 1;                                //  PWM1L2,PWM1H2 are in independent running mode
+     PWM1CON1bits.PMOD3 = 1;                                //  PWM1L3,PWM1H3 are in independent running mode
      
-     PWM1CON1bits.PEN1L = 0;        //  PWM1L1 NORMAL I/O
-     PWM1CON1bits.PEN1H = 1;        //  PWM1H1 PWM OUTPUT
-     PWM1CON1bits.PEN2L = 0;        //  PWM1L2 NORMAL I/O
-     PWM1CON1bits.PEN2H = 1;        //  PWM1H2 PWM OUTPUT
-     PWM1CON1bits.PEN3L = 0;        //  PWM1L3 NORMAL I/O
-     PWM1CON1bits.PEN3H = 1;        //  PWM1H3 PWM OUTPUT
+     PWM1CON1bits.PEN1L = 0;                                //  PWM1L1 NORMAL I/O
+     PWM1CON1bits.PEN1H = 1;                                //  PWM1H1 PWM OUTPUT
+     PWM1CON1bits.PEN2L = 0;                                //  PWM1L2 NORMAL I/O
+     PWM1CON1bits.PEN2H = 1;                                //  PWM1H2 PWM OUTPUT
+     PWM1CON1bits.PEN3L = 0;                                //  PWM1L3 NORMAL I/O
+     PWM1CON1bits.PEN3H = 1;                                //  PWM1H3 PWM OUTPUT
      
  
-     //PWM mode and prescaler
-     //PWM1, MOTORS 0,1,2    
+     //PWM Mode and Pre-scaler
+     //PWM1,PWM2,PWM3,MOTOR1,MOTOR2,and MOTOR3    
      P1TCON = 0;                    //  Clear all bits (use defaults)
      P1TCONbits.PTMOD = 0b00;       //  Free-runing mode 
      P1TCONbits.PTCKPS = 0b11;      //  1:64 prescaler
@@ -180,9 +185,8 @@ void run_motor(char ch, char dir, char pow)
         else if(dir == 0x02){
             LATBbits.LATB0 = 0;
             LATBbits.LATB1 = 1;
-        }
-            //update duty cycle 
-    P1DC1 = (2UL*P1TPER+2)*pow/100;  
+        }    
+    P1DC1 = (2UL*P1TPER+2)*pow/100;  // Update duty cycle 
     }
     else if(ch == 0x02)
     {
@@ -194,9 +198,21 @@ void run_motor(char ch, char dir, char pow)
             LATBbits.LATB2 = 0;
             LATBbits.LATB3 = 1;
         }
-    P1DC2 = (2UL*P1TPER+2)*pow/100;
+    P1DC2 = (2UL*P1TPER+2)*pow/100;  // Update duty cycle 
     }
     else if(ch == 0x03)
+    {
+        if(dir == 0x01){
+            LATBbits.LATB4 = 1;
+            LATBbits.LATB5 = 0;
+        }
+        else if(dir == 0x02){
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 1;
+        }
+    P1DC3 = (2UL*P1TPER+2)*pow/100;  // Update duty cycle 
+    }
+    else if(ch == 0x66)
     {
         if(dir == 0x01){
             LATBbits.LATB0 = 1;
@@ -210,10 +226,32 @@ void run_motor(char ch, char dir, char pow)
             LATBbits.LATB2 = 0;
             LATBbits.LATB3 = 1;
         }
-     P1DC1 = (2UL*P1TPER+2)*pow/100;
-     P1DC2 = (2UL*P1TPER+2)*pow/100;
+     P1DC1 = (2UL*P1TPER+2)*pow/100; // Update duty cycle 
+     P1DC2 = (2UL*P1TPER+2)*pow/100; // Update duty cycle
+        
     }
-
+    else if(ch == 0x67)
+    {
+        if(dir == 0x01){
+            LATBbits.LATB0 = 1;
+            LATBbits.LATB1 = 0;
+            LATBbits.LATB2 = 1;
+            LATBbits.LATB3 = 0;
+            LATBbits.LATB4 = 1;
+            LATBbits.LATB5 = 0;
+        }
+        else if(dir == 0x02){
+            LATBbits.LATB0 = 0;
+            LATBbits.LATB1 = 1;
+            LATBbits.LATB2 = 0;
+            LATBbits.LATB3 = 1;
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 1;
+        }
+     P1DC1 = (2UL*P1TPER+2)*pow/100; // Update duty cycle 
+     P1DC2 = (2UL*P1TPER+2)*pow/100; // Update duty cycle
+     P1DC3 = (2UL*P1TPER+2)*pow/100; // Update duty cycle   
+    }
 }
 void stop_motor(char channel){
     switch(channel)
@@ -230,12 +268,22 @@ void stop_motor(char channel){
             LATBbits.LATB3 = 0;
             break;
         }
-        case 12:
+        case 0x66:
         {
             LATBbits.LATB0 = 0;
             LATBbits.LATB1 = 0;
             LATBbits.LATB2 = 0;
             LATBbits.LATB3 = 0;
+            break;
+        }
+        case 0x67:
+        {
+            LATBbits.LATB0 = 0;
+            LATBbits.LATB1 = 0;
+            LATBbits.LATB2 = 0;
+            LATBbits.LATB3 = 0;
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 0;
             break;
         }
         default:
@@ -244,6 +292,8 @@ void stop_motor(char channel){
             LATBbits.LATB1 = 0;
             LATBbits.LATB2 = 0;
             LATBbits.LATB3 = 0;
+            LATBbits.LATB4 = 0;
+            LATBbits.LATB5 = 0;
             break;
         } 
     }
@@ -251,47 +301,48 @@ void stop_motor(char channel){
 int main(void) {
     
     /* Set Port */
-    __setIO(0xFFFF, 0x0000, 0x0090); // Argument 1 Set all ports are Digital I/O, 
+    __setIO(0xFFFF, 0x0001, 0x0090); // Argument 1 Set all ports are Digital I/O, 
     // Argument 2 Set all ports A are output, and Argument 3 Set all ports B are output except RB4 and RB7 are input.
     
     /* Disable Global Interrupt*/
     GLOBAL_INT_DISABLE;
-    initPLL();
-    initPWM();
-    initADC();
+    initPLL();  // Initialize PLL at FCY = 40 MHz
+    initPWM();  // Initialize PWM
+    initADC();  // Initialize ADC
     
-    __ExINT12_MAPPING(0x01,0x04);
+    /* External Interrupt Configuration */
+    __ExINT_MAPPING(0x01,0x04);     // Remap External INT1 on RP4
     
-    INT0_ENABLE(1);
-    INT1_ENABLE(1);
-    INT0_TRIG_EDGE(0);
-    INT1_TRIG_EDGE(0);
+    INT0_ENABLE(1);                 // Enable INT0
+    INT1_ENABLE(1);                 // Enable INT1
+    INT0_TRIG_EDGE(0);              // Set INT0 Positive Edge
+    INT1_TRIG_EDGE(0);              // Set INT1 Positive Edge
     
     /* Timer1 Configuration */
-    TIMER1_PRESCALE(0b11);
-    TIMER1_PERIOD(15625);
-    TIMER1_INT_ENABLE(1);
-    TIMER1_INT_PRIORITY(3);
-    TIMER1_ON(1);
+    TIMER1_PRESCALE(0b11);          // Timer1 1:256 Prescaler
+    TIMER1_PERIOD(15625);           // Set period every 10 milliseconds
+    TIMER1_INT_ENABLE(1);           // Enable Timer1 Interrupt
+    TIMER1_INT_PRIORITY(3);         // Set Timer1 Interrupt Priority by 3
+    TIMER1_ON(1);                   // Enable Timer1 ON
     
-    /*enable global interrupt*/
+    /* Enable Global Interrupt */
     GLOBAL_INT_ENABLE;
     
     double ADC0;
     char __motor_duty;
     while(1)
     {
-        ADC0 = adc_value(0);
-        __motor_duty = (ADC0/1023.0)*100;
-        if(__STOPM1status)
+        ADC0 = adc_value(0);                            // Reading ADC Channel 0
+        __motor_duty = (ADC0/1023.0)*100;               // DAC to Duty Cycle Calculation
+        if(__STOPM1status)                              // If __STOPM1status = 1
         {
-            stop_motor(0x01);
-            printf("Motor1 has been stopped\n");
+            stop_motor(ALL3);                           // Stop 3 Motor
+            printf("All Motor has been stopped\n");
         }
         else
         {
-            run_motor(0x01, __dir, __motor_duty);
-            printf("M1DIR: %1d, ADC0: %4.2f, SPD: %3d\n", __dir, ADC0, __motor_duty);
+            run_motor(0x01, __dir, __motor_duty);       // Run Motor1
+            printf("M1DIR: %1d, ADC0: %4.2f, SPD: %3d, TP1: %6d, TP1ON: %6d\n", __dir, ADC0, __motor_duty, P1TPER, P1DC1/2);
         }
     }
     
